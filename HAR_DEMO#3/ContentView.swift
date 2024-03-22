c4e3ecf21aa1f1e27481e5ecedcf4deb02bcc793
jose <jla238@miami.edu>
2024-03-21 20:23:54 -0400

    Initial Commit

---
+++ HAR_DEMO#3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HAR_DEMO#3
+//
+//  Created by José Abrams on 3/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HAR_DEMO#3/HAR_DEMO_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HAR_DEMO_3App.swift
+//  HAR_DEMO#3
+//
+//  Created by José Abrams on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HAR_DEMO_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
